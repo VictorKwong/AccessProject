b20458161b8b013f96ea9ef07afb7b3b8b8d521b
victorkaiwong <victorkaihong@gmail.com>
2023-05-02 23:55:12 -0400

    Add auth initialize:

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ Purpose: See if it is updated on the README file
 
 This project is to focus on login system, hashing password, and storing information in the back-end system MongoDB.
 
+- express
+- express-handlebars
+- client-sessions
+- mongoose
+- bcryptjs
+
+
 
--- auth-service.js
+++ auth-service.js
@@ -1,9 +1,30 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-function Rock() {
-    console.log("reset")
-    return new Promise(function(resolve, reject){
-        resolve();
-    })
-}
+const bcrypt = require('bcryptjs');
 
-module.exports = { Rock };
+var userSchema = new Schema({
+    "userName":  {
+      type: String,
+      "unique": true
+    },
+    "password": String
+});
+
+let User;
+
+function initialize () {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection("mongodb+srv://Victor:7OkL03vI5PTE9FlJ@lentil.1fev0.mongodb.net/test");
+  
+        db.on('error', (err)=>{
+            reject(err);
+        });
+        db.once('open', ()=>{
+           User = db.model("users", userSchema);
+           resolve();
+        });
+    });
+  };
+
+module.exports = { initialize };
